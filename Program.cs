c19b3e789625785a9a54ea956c42b831bbce45ac
Harrison O'Leary <145754861+hxrrybarry@users.noreply.github.com>
2024-02-29 14:37:02 +0000

    Update Program.cs
    Fixed physics by switching to a floating point when calculating X and Y positions, and only rounding to an integer value when detecting collisions and rendering.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace bouncymcball;
 
@@ -6,8 +6,10 @@ internal class Program
 {
     public static void Main()
     {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         Scene scene = new(27, 120);
-        RigidBody ball = new(scene, scene.XSize / 2, scene.YSize / 2, 'O', 4f, 0.3f);
+        // RigidBody ball = new(scene, scene.XSize / 2, scene.YSize / 2, 'γ', 4f, 0f, 0.9f);
 
         VoxelNoise map = new(scene.XSize, scene.YSize, 130, 3.5f, '#', new Random().Next());
         map.GenerateGrid();
@@ -15,13 +17,26 @@ internal class Program
         foreach (Voxel v in map.PlacedVoxels)
             scene.Populate(v);
 
+        for (int i = 0; i < 20; i++)
+        {
+            RigidBody ball = new RigidBody(scene, new Random().Next(0, scene.XSize), new Random().Next(0, scene.YSize), 'O', 4f, 0.1f, 0.3f);
+            ball.AddForce([new Random().Next(-300, 300) / 100f, new Random().Next(-300, 300) / 100f]);
+            scene.Populate(ball);
+        }
+
         for (int i = 0; i < scene.YSize; i++)
+        {
             scene.Populate(new Voxel(scene.XSize - 1, i, '#'));
+            scene.Populate(new Voxel(0, i, '#'));
+        }
+         
+        for (int i = 0; i < scene.XSize; i++)
+        {
+            scene.Populate(new Voxel(i, scene.YSize - 1, '#'));
+            scene.Populate(new Voxel(i, 0, '#'));
+        }
 
-        scene.Populate(ball);
         scene.Update();
-
-        ball.AddForce([new Random().Next(-200, 200) / 100f, new Random().Next(-200, 200) / 100f]);
 
         Console.ReadKey();
         // physics loop
